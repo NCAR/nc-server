e895c50f7a2daecb7e3530c69ff0046d570f0964
Gordon Maclean <maclean@ucar.edu>
2016-04-25 11:46:13 -0600

    Updates for building RPMs from git

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,86 +1,85 @@
 #!/bin/sh
 
-script=`basename $0`
-
-pkg=nc_server
-
-install=false
-
-while [ $# -gt 0 ]; do
-    case $1 in
-        -i)
-            install="true"
-            ;;
-    esac
-    shift
-done
-
-source repo_scripts/repo_funcs.sh
-
-topdir=${TOPDIR:-`get_rpm_topdir`}
-
-sourcedir=$(rpm --define "_topdir $topdir" --eval %_sourcedir)
-[ -d $sourcedir ] || mkdir -p $sourcedir
-
-rroot=`get_eol_repo_root`
-
-log=/tmp/$script.$$
-trap "{ rm -f $log; }" EXIT
+script=$(basename $0)
 
 set -o pipefail
 
-get_version() 
-{
-    awk '/^Version:/{print $2}' $1
-}
+pkg=nc_server
 
-get_release() 
-{
-    # discard M,S,P, mixed versions
-    v=$(svnversion $1 | sed 's/:.*$//' | sed s/[A-Z]//g)
-    [ $v == exported ] && v=1
-    echo $v
-}
+[ $# -gt 0 ] && dest=$1
 
-version=`get_version ${pkg}.spec`
+topdir=${TOPDIR:-$(rpmbuild --eval %_topdir)_$(hostname)}
+sourcedir=$(rpm --define "_topdir $topdir" --eval %_sourcedir)
+[ -d $sourcedir ] || mkdir -p $sourcedir
 
-# jenkins sets SVN_REVISION
-release=${SVN_REVISION:=$(get_release .)}
+log=$(mktemp /tmp/${script}_XXXXXX)
+tmpspec=`mktemp /tmp/${script}_XXXXXX.spec`
+awkcom=`mktemp /tmp/${script}_XXXXXX.awk`
 
-# tar 1.15.1 doesn't have --transform option
-tarversion=$(tar --version | cut -d \  -f 4)
-if echo $tarversion | fgrep -q 1.15; then
-    cd ..
-    tar czf $sourcedir/${pkg}-${version}.tar.gz --exclude .svn \
-	nc_server/SC* nc_server/nc_server.h nc_server/*.cc \
-	nc_server/nc_check.c nc_server/*.x \
-	nc_server/site_scons nc_server/scripts \
-	nc_server/etc nc_server/usr nc_server/systemd || exit $?
-    cd -
-else
-    # use --transform to put the package name in the tar path names
-    tar czf $sourcedir/${pkg}-${version}.tar.gz --exclude .svn --transform="s,./,$pkg/," \
-	./SConstruct ./nc_server.h ./*.cc ./nc_check.c ./nc_server_rpc.x \
-	./site_scons ./scripts ./etc ./usr ./systemd || exit $?
+trap "{ rm -f $log $tmpspec $awkcom; }" EXIT
+
+if ! gitdesc=$(git describe --match "v[0-9]*"); then
+    echo "git describe failed, looking for a tag of the form v[0-9]*"
+    gitdesc="v1.0"
+    # exit 1
 fi
+
+# example output of git describe: v2.0-14-gabcdef123
+gitdesc=${gitdesc/#v}       # remove leading v
+version=${gitdesc%%-*}       # 2.0
+
+release=${gitdesc#*-}       # 14-gabcdef123
+release=${release%-*}       # 14
+[ $gitdesc == "$release" ] && release=0 # no dash
+
+# run git describe on each hash to create a version
+cat << \EOD > $awkcom
+/^[0-9a-f]{7}/ {
+    cmd = "git describe --match '[vV][0-9]*' " $0 " 2>/dev/null"
+    res = (cmd | getline version)
+    close(cmd)
+    if (res == 0) {
+        version = ""
+    }
+}
+/^\*/ { print $0,version }
+/^-/ { print $0 }
+/^$/ { print $0 }
+EOD
+
+# create change log from git log messages.
+# Put SHA hash by itself on first line. Above awk script then
+# converts it to the output of git describe, and appends it to "*" line.
+# Truncate subject line at 60 characters 
+# git convention is that the subject line is supposed to be 50 or shorter
+git log --max-count=100 --date-order --format="%H%n* %cd %aN%n- %s%n" --date=local . | \
+    sed -r 's/[0-9]+:[0-9]+:[0-9]+ //' | sed -r 's/(^- .{,60}).*/\1/' | \
+    awk --re-interval -f $awkcom | cat ${pkg}.spec - > $tmpspec
+
+cd ..
+
+tar czf $sourcedir/${pkg}-${version}.tar.gz --exclude .svn --exclude .git \
+    nc_server/SC* nc_server/nc_server.h nc_server/*.cc \
+    nc_server/nc_check.c nc_server/*.x \
+    nc_server/scripts \
+    nc_server/etc nc_server/usr nc_server/systemd || exit $?
+cd -
 
 rpmbuild -v -ba \
     --define "_topdir $topdir"  \
-    --define "release $release" \
+    --define "gitversion $version" --define "releasenum $release" \
     --define "debug_package %{nil}" \
-    ${pkg}.spec | tee -a $log  || exit $?
+    $tmpspec | tee -a $log  || exit $?
 
 echo "RPMS:"
 egrep "^Wrote:" $log
 rpms=`egrep '^Wrote:' $log | egrep /S?RPMS/ | awk '{print $2}'`
 echo "rpms=$rpms"
 
-if $install && [ -d $rroot ]; then
-    echo "Moving rpms to $rroot"
-    copy_rpms_to_eol_repo $rpms
-elif $install; then
-    echo "$rroot not found. Leaving RPMS in $topdir"
-    else
-    echo "-i option not specified. RPMS will not be installed in $rroot"
+if [ -n "$dest" -a -d "$dest" ]; then
+    echo "Moving rpms to $dest"
+    move_rpms_to_eol_repo $rpms
+else
+    echo "$dest not found. Leaving RPMS in $topdir"
 fi
 
--- nc_server.spec
+++ nc_server.spec
@@ -3,8 +3,8 @@
 
 Summary: Server for NetCDF file writing.
 Name: nc_server
-Version: 1.0
-Release: %{release}%{?dist}
+Version: %{gitversion}
+Release: %{releasenum}%{?dist}
 License: GPL
 Group: Applications/Engineering
 Url: http://www.eol.ucar.edu/
@@ -12,12 +12,9 @@ Packager: Gordon Maclean <maclean@ucar.edu>
 # Allow this package to be relocatable to other places than /opt/nc_server
 # rpm --relocate /opt/nc_server=/usr
 Prefix: /opt/nc_server
-
-# becomes RPM_BUILD_ROOT, except on newer versions of rpmbuild
-BuildRoot:  %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildRequires: netcdf-devel libcap-devel nidas-devel eol_scons
 Vendor: UCAR
 Source: %{name}-%{version}.tar.gz
-BuildRequires: netcdf-devel libcap-devel nidas-devel
 Requires: nc_server-clients
 %description
 Server for NetCDF file writing.
@@ -112,47 +109,3 @@ rm -rf $RPM_BUILD_ROOT
 %config(noreplace) %{_sysconfdir}/profile.d/nc_server.csh
 
 %changelog
-* Fri May 22 2015 Gordon Maclean <maclean@ucar.edu> 1.0-620
-- Improved exception handling for invalid directory, interval and
-- file length.
-* Sat Feb  7 2015 Gordon Maclean <maclean@ucar.edu> 1.0-590
-- Add cap_setgid,cap_net_bind_service capabilties to nc_server executable.
-- With this one doesn't need sudo.
-- Add systemd/user service file and README for how to run from systemd.
-* Mon Apr  2 2012 Gordon Maclean <maclean@ucar.edu> 1.0-12
-- Create counts variable in existing variable group if necessary.
-* Mon Jan  2 2012 Gordon Maclean <maclean@ucar.edu> 1.0-11
-- Fix -Weffc++ warnings. Don't set process timezone to GMT.
-- Always update units attributes on connection.
-* Tue Oct 18 2011 Gordon Maclean <maclean@ucar.edu> 1.0-10
-- Put client programs in nc_server-clients.
-- Path setting in /etc/profile.d/nc_server.*sh is commented out,
-- so by default users must add nc_server to their own path.
-- /etc/init.d/nc_server sets its path.
-* Sun Oct 16 2011 Gordon Maclean <maclean@ucar.edu> 1.0-9
-- Everything now installed to /opt/nc_server, and always to lib, not lib64.
-* Thu Oct 13 2011 Gordon Maclean <maclean@ucar.edu> 1.0-8
-- Improved error handling in definedatarec call.
-* Tue Oct 11 2011 Gordon Maclean <maclean@ucar.edu> 1.0-7
-- Added CHECKERROR procedure for batch writers to use instead of NULLPROC.
-* Sun Oct  2 2011 Gordon Maclean <maclean@ucar.edu> 1.0-6
-- Reduced SYNC_CHECK_INTERVAL from 60 to 5 seconds.
-- Cleaned up nc_server.check, which now runs /etc/init.d/nc_server
-- instead of nc_server directly.
-* Sun Aug 21 2011 Gordon Maclean <maclean@ucar.edu> 1.0-5
-- Fix usage of getpwnam_r, getgrnam_r, getgrid_r.
-* Wed Aug 10 2011 Gordon Maclean <maclean@ucar.edu> 1.0-4
-- Keep incrementing connecionId to avoid the possibility of a paused process
-- attempting writes after the connection has timed out and the id has been
-- given to another process.
-- Much rework of counts variable handling.
-- Fix bug in /etc/init.d/nc_server stop - was killing itself.
-* Fri Apr 22 2011 Gordon Maclean <maclean@ucar.edu> 1.0-3
-- Support more than one -g option to add supplemental group ids to this process.
-- Add SET_SETGID capability for the setgroups() call.
-- Report strerror_r() if ncerror.get_err() is 0.
-* Fri Apr 15 2011 Gordon Maclean <maclean@ucar.edu> 1.0-2
-- added /etc/init.d/nc_server boot script. %pre does useradd/groupadd of nidas.eol.
-- nc_server has -g runstring option to set the group. sudo is not needed to start.
-* Mon Jun  7 2010 Gordon Maclean <maclean@ucar.edu> 1.0-1
-- original
